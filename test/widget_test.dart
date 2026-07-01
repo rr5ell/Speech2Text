d0338aeea00e5b10da59688e9548aaace9e8cf00
HS <1580103545@qq.com>
2026-07-01 09:48:37 +0800

    feat: add korean hotword variants

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,6 +4,6 @@ import 'package:speech_to_text_sherpa/main.dart';
 void main() {
   testWidgets('App loads home screen', (WidgetTester tester) async {
     await tester.pumpWidget(const SpeechToTextApp());
-    expect(find.text('Speech_to_Text'), findsOneWidget);
+    expect(find.text('Speed_to_Text'), findsOneWidget);
   });
 }
